b72bff0ee7b87e150c3cc8fdbf085d027ad15169
Jotsarup Singh Narula <110384097+GitJotsarup@users.noreply.github.com>
2023-11-15 23:35:12 +0700

    Add files via upload

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,36 @@
+<script setup>
+defineProps({
+  pokemon: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <RouterLink :to="{ name: 'pokemon-details', params: { id: pokemon.id } }">
+    <div class="pokemon-card">
+      <img :src="pokemon.image" alt="Pokemon Image" />
+      <h2>{{ pokemon.name }}</h2>
+      <span>Type 1: {{ pokemon.type1 }}, Type 2: {{ pokemon.type2 }}</span>
+      <p>{{ pokemon.evolution }}</p>
+    </div>
+  </RouterLink>
+</template>
+
+<style scoped>
+.pokemon-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 10px solid #ffd700;
+  background-color: #7f839b;
+  margin-bottom: 18px;
+  border-radius: 15px;
+}
+
+.pokemon-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
---
+++ src/views/PokemonListView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import PokemonCard from '@/components/PokemonCard.vue';
+import PokemonService from '@/services/PokemonService.js';
+
+const pokemons = ref(null);
+
+onMounted(async () => {
+  PokemonService.getPokemons()
+  .then((response) => {
+    pokemons.value = response.data
+  })
+  .catch((error) => {
+    console.error(error);
+  })
+});
+</script>
+
+<template>
+  <div class="pokemon-list">
+    <PokemonCard v-for="pokemon in pokemons" :key="pokemon.id" :pokemon="pokemon" />
+  </div>
+</template>
