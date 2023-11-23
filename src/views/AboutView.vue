932135ae27597e480a09ffba6ed2ec9c2e5b9e3b
Jotsarup Singh Narula <110384097+GitJotsarup@users.noreply.github.com>
2023-11-23 22:53:32 +0700

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,43 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { db } from '@/js/firebase.js'
+// import { collection, getDocs } from "firebase/firestore";
+import { collection, onSnapshot } from "firebase/firestore"
+
+import PokemonCard from '@/components/PokemonCard.vue';
+
+// create an empty array
+const pokemons = ref([])
+
+async function getPokemons() {
+  onSnapshot(collection(db, 'pokemons'), (querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+      let pokemon = {
+        "id": doc.id,
+        "type1": doc.data().type1,
+        "type2": doc.data().type2,
+        "evolution": doc.data().evolution,
+        "jname": doc.data().jname,
+        "jotfav": doc.data().jotfav,
+        "image": doc.data().image
+      }
+      // add the object to the array
+        pokemons.value.push(pokemon)
+  });
+})
+}
+
+onMounted(async () => {
+    console.log("Connecting to Firebase")
+    await getPokemons()
+})
+
+
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is a PokeDex website that stores the information of pokemon.</h1>
+  <div class="pokemons">
+    <PokemonCard v-for="pokemon in pokemons" :key="pokemon.id" :pokemon="pokemon" />
   </div>
 </template>
 
