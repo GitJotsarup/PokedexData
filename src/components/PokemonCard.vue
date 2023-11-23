c24e745628411aefc951badc466eb0de4d32607e
Jotsarup Singh Narula <110384097+GitJotsarup@users.noreply.github.com>
2023-11-23 22:51:40 +0700

    Update PokemonCard.vue

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -9,17 +9,22 @@ defineProps({
 
 <template>
   <RouterLink :to="{ name: 'pokemon-details', params: { id: pokemon.id } }">
-    <div class="pokemon-card">
+    <div :class="{ 'pokemon-card': true, 'favorite-pokemon': pokemon.jotfav }">
       <img :src="pokemon.image" alt="Pokemon Image" />
-      <h2>{{ pokemon.name }}</h2>
-      <span>Type 1: {{ pokemon.type1 }}, Type 2: {{ pokemon.type2 }}</span>
-      <p>{{ pokemon.evolution }}</p>
+      <div class="pokemon-details">
+        <h2>{{ pokemon.name }}</h2>
+        <span>Type 1: {{ pokemon.type1 }}, Type 2: {{ pokemon.type2 }}</span>
+        <p>{{ pokemon.evolution }}</p>
+      </div>
     </div>
   </RouterLink>
 </template>
 
 <style scoped>
 .pokemon-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   padding: 20px;
   width: 250px;
   cursor: pointer;
@@ -27,6 +32,21 @@ defineProps({
   background-color: #7f839b;
   margin-bottom: 18px;
   border-radius: 15px;
+}
+
+.favorite-pokemon {
+  transform: scale(1.01);
+  box-shadow: 0 0px 20px 0 rgba(255, 0, 0, 0.925)
+}
+
+.pokemon-card img {
+  max-width: 100%;
+  border-radius: 10px;
+}
+
+.pokemon-details {
+  text-align: center;
+  margin-top: 10px;
 }
 
 .pokemon-card:hover {
